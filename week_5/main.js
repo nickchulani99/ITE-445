f259a06f5b72aeb4026340004b33a08e4fbbf684
“Nitesh <nickchulani99@gmail.com>
2016-08-25 12:00:13 +0700

    random coins

--- week_5/main.js
+++ week_5/main.js
@@ -12,9 +12,12 @@ var mainState = {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.renderer.renderSession.roundRixels = true;
 
-		//coin
+		//display coin
 		this.coin = game.add.sprite(60,140,'coin');
+
+		//add  arcade physic to coin 
 		game.physics.arcade.enable(this.coin);
+		//set anchor 
 		this.coin.anchor.setTo(0.5,0.5);
 
 		//player
@@ -32,11 +35,11 @@ var mainState = {
 		this.walls = game.add.group();
 		this.walls.enableBody = true;
 
-		//all vertical
+		//2 walls in a group left, right
 		game.add.sprite(0,0,'wallV',0,this.walls);
 		game.add.sprite(480,0,'wallV',0,this.walls);
 
-		//all horizontal
+		//walls
 		game.add.sprite(0,0,'wallH',0,this.walls); 
 		game.add.sprite(300,0,'wallH',0,this.walls);
 		game.add.sprite(0,320,'wallH',0,this.walls);
@@ -45,20 +48,22 @@ var mainState = {
 		game.add.sprite(-100,160,'wallH',0,this.walls);
 		game.add.sprite(400,160,'wallH',0,this.walls);
 
-		//middle horizontals scaled
+		
 		var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
 		middleTop.scale.setTo(1.5, 1);
 
 		var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls);
 		middleBottom.scale.setTo(1.5, 1);
 
+		// set wall to immovable	
 		this.walls.setAll('body.immovable', true);
 
-		//score
+		//display score
 		this.scoreLabel = game.add.text(30,30,'Score: 0', {
 			font: '18px Arial',
 			fill: '#ffffff'
 		});
+		//initialize score
 		this.score = 0;
 	},
 
@@ -94,12 +99,35 @@ var mainState = {
 	playerDie: function(){
 		game.state.start('main');
 	},
+	updateCoinPosition:function(){
+		var coinPosition =[
+		{x:140, y:60}, {s:360, y:60} ,
+		{x:60, y:140}, {x:440, y:140},
+		{x:130, y:300},{x:370, y :300}
+		];
+		for (var i=0; i<coinPosition.length; i++){
+			if(coinPosition[i].x==this.coin.x){
+				coinPosition.splice(i,1);
+			}
+		}
+		var newPosition = game.rnd.pick(coinPosition);
+
+		this.coin.reset(newPosition.x, newPosition.y);
+	},
 
 	takeCoin: function(player, coin) {
-		this.coin.kill();
+		// var number = game.rnd.integerInRange(a,b);
+		// this.coin.reset(x,y);
+		
+		//increment score 
 		this.score += 5;
+		//update score 
 		this.scoreLabel.text = 'Score: ' + this.score;
+		//change position
+		this.updateCoinPosition();
 	},
+
+	
 };
 	
 var game = new Phaser.Game(500,	340, Phaser.AUTO, 'gameDiv');
